8e32b8ea5fae0084a86559461c11e6c5264f69f2
Johanna Misaela <116675181+JohanaMisaela@users.noreply.github.com>
2023-01-29 22:15:28 +0300

    acceuil

---
+++ front/src/layouts/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './../assets/css/header.css'
+function Header() {
+  return (
+    <div>
+        
+        <div class="main">
+  <h1>This is purely: <div class="roller">
+    <span id="rolltext">HTML<br/>
+    CSS<br/>
+    Curiosity<br/>
+
+    <span id="spare-time">too much spare time?</span><br/></span>
+    </div>
+    </h1>
+    
+  </div>
+  
+    
+    </div>
+  )
+}
+
+export default Header
--- front/src/pages/Acceuil.js
+++ front/src/pages/Acceuil.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../layouts/Navbar'
+import Header from '../layouts/Header'
 import './../assets/css/Navbar.css'
 function Acceuil() {
   return (
     <div>
         <Navbar/>
+        <Header/>
     </div>
   )
 }
